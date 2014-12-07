4aa2042cb51540df2a4aa96f9dffa7b1c6aa8e82
Remi Rampin <remirampin@gmail.com>
2014-12-07 01:03:15 -0500

    Fixes for Windows

--- java-jna/c/c_wrapper.cpp
+++ java-jna/c/c_wrapper.cpp
@@ -1,7 +1,10 @@
 #include "cpplib/MyClass.h"
 
-#define DLL_EXPORT
-/* #define DLL_EXPORT __declspec(dllexport) */
+#ifdef _WIN32
+#   define DLL_EXPORT __declspec(dllexport)
+#else
+#   define DLL_EXPORT
+#endif
 
 extern "C" DLL_EXPORT MyClass *MyClass_ctor(int nb);
 MyClass *MyClass_ctor(int nb)
